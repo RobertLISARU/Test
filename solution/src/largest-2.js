dee67750772b989c6d7e48c5be491d5261fe9d80
Robert Lisaru <lisaru.robert@hotmail.com>
2018-09-27 21:06:59 +0300

    Change the largest2 implementation to O(n) complexity
    This implementation is longer, but only iterates one time through the array.

--- solution/src/largest-2.js
+++ solution/src/largest-2.js
@@ -1,6 +1,14 @@
 function largest2(numbers) {
-    let max = Math.max(...numbers);
-    let max2 = Math.max(...numbers.filter((x)=>x!=max));
+    let max = Number.MIN_VALUE;
+    let max2 = Number.MIN_VALUE;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max2 = max;
+            max = numbers[i];
+        } else if (numbers[i] > max2) {
+            max2 = numbers[i];
+        }
+    }
     return [max, max2];
 }
 
