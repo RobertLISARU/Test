0d0c716024591acea0d40ee52f827021b416ed44
Robert Lisaru <lisaru.robert@hotmail.com>
2018-09-27 20:54:09 +0300

    Fix wrong solution to the largest 2 values problem
    This solution is shorter, but has O(3n) complexity, iterating the array 3 times.

--- solution/src/largest-2.js
+++ solution/src/largest-2.js
@@ -1,12 +1,7 @@
 function largest2(numbers) {
-    let largest = [Number.MIN_VALUE, Number.MIN_VALUE];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > largest[0]) {
-            largest[1] = largest[0];
-            largest[0] = numbers[i];
-        }
-    }
-    return largest;
+    let max = Math.max(...numbers);
+    let max2 = Math.max(...numbers.filter((x)=>x!=max));
+    return [max, max2];
 }
 
 module.exports = largest2;
