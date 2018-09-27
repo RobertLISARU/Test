ef93a1fab8f32a28961a2a8dfbe1ca60fa079915
Lisaru Robert <lisaru.robert@hotmail.com>
2018-09-27 04:00:38 +0300

    1st problem: neater solution in functional style. But now is tricky (for me) to do the input validation that checks for non-integers in the array (in a clean way, at least).

--- solution/src/sum-evens.js
+++ solution/src/sum-evens.js
@@ -2,14 +2,7 @@ function sumEvens(numbers) {
     if (!Array.isArray(numbers)) {
         return undefined;
     }
-    sum = 0;
-    for (i = 0; i < numbers.length; i++) {
-        if (!Number.isInteger(numbers[i])) {
-            return undefined;
-        }
-        sum += numbers[i] % 2 === 0 ? numbers[i] : 0;
-    }
-    return sum;
+    return numbers.filter((x) => x % 2 == 0).reduce((x, y) => x + y, 0);
 }
 
 module.exports = sumEvens;
