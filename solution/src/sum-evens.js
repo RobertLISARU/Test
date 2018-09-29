760ea6f8ed0b5a626c40e6bef5dd7843b0f36b8b
Robert Lisaru <lisaru.robert@hotmail.com>
2018-09-29 18:03:26 +0300

    Rename parameters for reduce function to be even more expressive

--- solution/src/sum-evens.js
+++ solution/src/sum-evens.js
@@ -1,6 +1,6 @@
 function sumEvens(numbers) {
     return numbers.filter((number) => number % 2 == 0)
-        .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+        .reduce((previousSum, currentValue) => previousSum + currentValue, 0);
 }
 
 module.exports = sumEvens;
