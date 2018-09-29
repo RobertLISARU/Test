1c721ad8bc5398c0611af281eca20ddc533e1330
Robert Lisaru <lisaru.robert@hotmail.com>
2018-09-29 17:51:48 +0300

    Add test case for largest2 when negative numbers

--- solution/test/test-largest-2.js
+++ solution/test/test-largest-2.js
@@ -14,3 +14,10 @@ it("largest 2 extra scenario", () => {
    assert.equal(largest[0], 5);
    assert.equal(largest[1], 4);
 });
+
+it("largest 2 when negative numbers", () => {
+    const numbers = [-1, -2, 0, 4, 3];
+    let largest = largest2(numbers);
+    assert.equal(largest[0], 4);
+    assert.equal(largest[1], 3);
+});
